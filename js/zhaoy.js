b07d42847fac1cea18b95ce4142336d7bd41f368
zhaoy875 <1174499602@qq.com>
2016-09-18 23:33:11 +0800

    update

--- js/zhaoy.js
+++ js/zhaoy.js
@@ -9,77 +9,73 @@ console.group();
 console.groupEnd();
 
 	! function(e, t, a) {
-		// 定义任务
-		function n() {
-			c(heart_css),
-				o(),
-				r()
-		}
+    	// 定义任务
+        function n() {c(heart_css),
+                o(),
+                r()
+        }
 
-		function r() {
-			for (var e = 0; e < d.length; e++)
-				d[e].alpha <= 0 ? (t.body.removeChild(d[e].el),
-					d.splice(e, 1)) : (d[e].y--,
-					//d[e].scale += .004,
-					// d[e].alpha -= .013,//层次效果递进
-					d[e].scale += .008,
-					d[e].alpha -= .015,
-					d[e].el.style.cssText = "left:" + d[e].x + "px;top:" + d[e].y + "px;opacity:" + d[e].alpha + ";transform:scale(" + d[e].scale + "," + d[e].scale + ") rotate(45deg);background:" + d[e].color + ";z-index:99999");
-			requestAnimationFrame(r)
-		}
+        function r() {
+            for (var e = 0; e < d.length; e++)
+                d[e].alpha <= 0 ? (t.body.removeChild(d[e].el),
+                    d.splice(e, 1)) : (d[e].y--,
+                    //d[e].scale += .004,
+                    // d[e].alpha -= .013,//效果递进
+                    d[e].scale += .008,
+                    d[e].alpha -= .015,
+                    d[e].el.style.cssText = "left:" + d[e].x + "px;top:" + d[e].y + "px;opacity:" + d[e].alpha + ";transform:scale(" + d[e].scale + "," + d[e].scale + ") rotate(45deg);background:" + d[e].color + ";z-index:99999");
+            requestAnimationFrame(r)
+        }
 
-		// 点击事件
-		function o() {
-			var t = "function" == typeof e.onclick && e.onclick;
-			e.onclick = function(e) {
-				t && t(),
-					i(e)
-			}
-		}
+        // 点击事件
+        function o() {
+            var t = "function" == typeof e.onclick && e.onclick;
+            e.onclick = function(e) {
+                t && t(),
+                    i(e)
+            }
+        }
 
-		function i(e) {
-			// var wraper = t.createElement("div");
-			var a = t.createElement("div");
-			a.innerHTML = '<div class="heart111"></div>';
-			a.className = "wrap";
+        function i(e) {
+            var a = t.createElement("div");
+            a.className = "heart",
+                d.push({
+                    el: a,
+                    x: e.clientX - 5,
+                    y: e.clientY - 5,
+                    scale: 1,
+                    alpha: 1,
+                    color: s()
+                }),
+                t.body.appendChild(a)
+        }
+        // 创建样式标签style
+        function c(e) {
+            var a = t.createElement("style");
+            a.type = "text/css";
+            try {
+                a.appendChild(t.createTextNode(e))
+            } catch (n) {
+                a.styleSheet.cssText = e
+            }
+            t.getElementsByTagName("head")[0].appendChild(a)
+        }
+        // 生成随机颜色
+        function s() {
+        	// ~是按位取反运算（经过位运算的都会自动变成整数，去除小数点），~~是取反两次
+            return "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ")"
+        }
 
-			d.push({
-					el: a,
-					x: e.clientX - 5,
-					y: e.clientY - 5,
-					scale: 1,
-					alpha: 1,
-					color: s()
-				}),
-				t.body.appendChild(a)
-		}
-		// 创建样式标签style
-		function c(e) {
-			var a = t.createElement("style");
-			a.type = "text/css";
-			try {
-				a.appendChild(t.createTextNode(e))
-			} catch (n) {
-				a.styleSheet.cssText = e
-			}
-			t.getElementsByTagName("head")[0].appendChild(a)
-		}
-		// 生成随机颜色
-		function s() {
-			// ~是按位取反运算（经过位运算的都会自动变成整数，去除小数点），~~是取反两次
-			return "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ")"
-		}
+        // 星星css样式
+        var heart_css=".heart{width: 12px;height: 12px;margin:1em;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -6px;}.heart:before{left: -6px;}";
 
-		// 星星css样式
-		var heart_css = ".wrap{position:fixed;margin:5em;transform:rotate(45deg);-webkit-transform:rotate(45deg);-moz-transform:rotate(45deg);transform-origin:center center}.heart111{width:80px;height:80px;position:relative;background:#000}.heart111:after,.heart111:before{content:'';width:inherit;height:inherit;position:absolute;top:0;left:0;background:inherit;border-radius:50%;-webkit-border-radius:50%;-moz-border-radius:50%}.heart111:after{top:-40px}.heart111:before{left:-40px}";
-
-		var d = [];
-		e.requestAnimationFrame = function() {
-				return e.requestAnimationFrame || e.webkitRequestAnimationFrame || e.mozRequestAnimationFrame || e.oRequestAnimationFrame || e.msRequestAnimationFrame || function(e) {
-					setTimeout(e, 1e3 / 60)
-				}
-			}(),
-			n()
-			// n()执行任务
-	}(window, document);
+        var d = [];
+        e.requestAnimationFrame = function() {
+                return e.requestAnimationFrame || e.webkitRequestAnimationFrame || e.mozRequestAnimationFrame || e.oRequestAnimationFrame || e.msRequestAnimationFrame || function(e) {
+                    setTimeout(e, 1e3 / 60)
+                }
+            }(),
+            n()
+          // n()执行任务
+    }(window, document);
 })();
