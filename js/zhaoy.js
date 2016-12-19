0f89ca91f9e30e65e3cb523b4c5c9dbdf301bf56
zhaoy <1174499602@qq.com>
2016-12-20 05:32:29 +0800

    update

--- js/zhaoy.js
+++ js/zhaoy.js
@@ -79,4 +79,25 @@
             n()
           // n()执行任务
     }(window, document);
+
+    //h5 visibilitychange
+     
+
+    var OriginTitile = document.title;
+    var titleTime;
+    document.addEventListener('visibilitychange', function() {
+        if (document.hidden) {
+            // $('[rel="icon"]').attr('href', "/img/TEP.ico");
+            document.title = '(●—●)天哪，崩溃啦！';
+            clearTimeout(titleTime);
+        } else {
+            // $('[rel="icon"]').attr('href', "/favicon.ico");
+            document.title = '(/≧▽≦/)逗你呢,又好咯~' + OriginTitile;
+            titleTime = setTimeout(function() {
+                document.title = OriginTitile;
+            }, 2000);
+        }
+    });
 })();
+
+
